65791204410ec037d86bd7758a00b16fb41f3e2c
TIMESGROUP\Pravish.Mittal <pravish.mittal@gaana.com>
2021-06-01 18:48:39 +0530

    Cucumber_Reporting

--- src/test/java/com/gaana/test/base/Test_Runner.java
+++ src/test/java/com/gaana/test/base/Test_Runner.java
@@ -11,9 +11,9 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = { "src/test/java/com/gaana/features" }, tags = "@Gaana", glue = {
+@CucumberOptions(features = { "src/test/java/com/gaana/features/Login" }, tags = "@Gaana", glue = {
 		"com.gaana.stepdefinitions","com.gaana.test.base" }, plugin = { "pretty",
-				"html:target/cucumber-reports", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
+				"html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
 
 public class Test_Runner extends AbstractTestNGCucumberTests {
 	
