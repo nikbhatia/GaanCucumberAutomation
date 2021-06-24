220989ae1de2164d1710054dfd89ed06791d6f2d
Pravish Mittal <pravish.mittal@gaana.com>
2021-06-24 07:25:41 +0000

    Test_Runner.java edited online with Bitbucket

--- src/test/java/com/gaana/test/base/Test_Runner.java
+++ src/test/java/com/gaana/test/base/Test_Runner.java
@@ -15,7 +15,7 @@ import cucumber.api.java.Before;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
-@CucumberOptions(features = { "src/test/java/com/gaana/features" }, tags = "@Gaana", glue = {
+@CucumberOptions(features = { "src/test/java/com/gaana/features/Hamburger" }, tags = "@Gaana", glue = {
 		"com.gaana.stepdefinitions","com.gaana.test.base" }, plugin = { "pretty",
 				"html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
 
@@ -56,7 +56,7 @@ public class Test_Runner extends AbstractTestNGCucumberTests {
 		er.startResultSummaryTable("testOverview");
 		er.updateResultSummaryTable();
 		
-		EmailableReport.out.println("</tr>");
+		//EmailableReport.out.println("</tr>");
 		
 		er.startScenarioSummaryTable("methodOverview");
 		EmailableReport.out.println(subject);
