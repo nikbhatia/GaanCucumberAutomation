3381b41bb82b4503cdcce10dd3cbf2ed77fcae52
Saurav Ghosh <saurav.ghosh@timesinternet.in>
2021-08-06 12:17:47 +0000

    URLBuilder.java edited online with Bitbucket

--- src/test/java/com/gaana/automation/util/URLBuilder.java
+++ src/test/java/com/gaana/automation/util/URLBuilder.java
@@ -30,7 +30,7 @@ public class URLBuilder {
         endPoints.put("androidTv","/tv");
         endPoints.put("quickSilverLogin","/cXVpY2tzaWx2ZXI=");
         endPoints.put("particularPodcast","/podcast/all-things-music-with-sarthak-2018");
-        endPoints.put("Lyricspage","/lyrics/tere-sang-yaara-1");
+        endPoints.put("Lyricspage","/lyrics/hum-to-mohabbat-karega-10");
 
         endPoints.put("Playlist","/playlist");
 
