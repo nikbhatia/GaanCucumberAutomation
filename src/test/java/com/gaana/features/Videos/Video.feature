f8e12791312743a217339c96b34c6e7bee34c270
Shivam Bhargav <shivam.bhargav@gaana.com>
2021-09-10 17:14:52 +0530

    gaanavideo_TestCases

---
+++ src/test/java/com/gaana/features/Videos/Video.feature
@@ -0,0 +1,77 @@
+
+Feature: Test Scenarios for video songs
+
+  @Gaana @Video
+  Scenario Outline: Verify video songs for GaanaPlus user
+    Given user opens the application "homePage"
+    Then user logins with "<loginType>"
+    When user opens the application "videoPage"
+    Then user clicks on video and verify the video title
+
+    Examples:
+      |loginType   |
+      |GaanaPlus   |
+
+
+  @Gaana @Video
+  Scenario Outline: Verify Videos play time on every video tab for GaanaPlus user
+    Given user opens the application "homePage"
+    Then user logins with "<loginType>"
+    When user opens the application "videoPage"
+    Then click on video and verify the video time
+
+    Examples:
+      |loginType   |
+      |GaanaPlus   |
+
+  @Gaana @Video
+  Scenario Outline: Verify video songs for Non-GaanaPlus user
+    Given user opens the application "homePage"
+    Then user logins with "<loginType>"
+    When user opens the application "videoPage"
+    Then user clicks on video and verify the video title
+
+    Examples:
+      |loginType   |
+      |NonGaanaPlus   |
+
+  @Gaana @Video
+  Scenario Outline: Verify Videos tabs on video page for Non-GaanaPlus user
+    Given user opens the application "homePage"
+    Then user logins with "<loginType>"
+    When user opens the application "videoPage"
+    Then user clicks on gaanavideo tabs
+
+    Examples:
+      |loginType   |
+      |NonGaanaPlus   |
+
+  @Gaana @Video
+  Scenario Outline: Verify Videos play time on every video tab for Non-GaanaPlus user
+    Given user opens the application "homePage"
+    Then user logins with "<loginType>"
+    When user opens the application "videoPage"
+    Then click on video and verify the video time
+
+    Examples:
+      |loginType   |
+      |NonGaanaPlus   |
+
+  @Gaana @Video
+  Scenario: Verify video songs for Non-loggedin user
+    Given user opens the application "homePage"
+    When user opens the application "videoPage"
+    Then user clicks on video and verify the video title
+
+  @Gaana @Video
+  Scenario: Verify Videos tabs on video page for Non-loggedin user
+    Given user opens the application "homePage"
+    When user opens the application "videoPage"
+    Then user clicks on gaanavideo tabs
+
+
+  @Gaana @Video
+  Scenario: Verify Videos play time on every video tab for Non-loggedin user
+    Given user opens the application "homePage"
+    When user opens the application "videoPage"
+    Then click on video and verify the video time
---
+++ src/test/java/com/gaana/pageobjects/Videos/VideosPO.java
@@ -0,0 +1,85 @@
+package com.gaana.pageobjects.Videos;
+
+import com.gaana.automation.util.GenericMethod;
+import com.gaana.test.base.BaseAutomation;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import java.util.List;
+
+public class VideosPO extends BaseAutomation {
+
+    GenericMethod generic = new GenericMethod();
+
+    private By VideoTabs = By.xpath("//div[@class='_ul']//a");
+    private By VideoSong = By.cssSelector("li:nth-child(1)>div>a");
+    private By forwardscroll = By.cssSelector("button.btn.btn_next.sm-hide");
+    private By hoverEle = By.xpath("//div[@class='_ul']");
+    private By videoStartime = By.xpath("//div[@id=\"vtimeplayed\"]");
+    private By videoEndtime = By.xpath("//div[@id=\"vtimefull\"]");
+    private By videoPage = By.xpath("//video[@id=\"gaanaVideoPlayer\"]");
+    private By selectedTab = By.xpath("//a[@class='link selected']//small");
+
+    public void videoSong_Play() {
+        String songsTitle = driver.findElement(VideoSong).getText();
+        wait.hardWait(3);
+        click(VideoSong);
+        wait.hardWait(5);
+        String PlaySongTitle = getPageTitle();
+        if (PlaySongTitle.contains(songsTitle)) {
+            Assert.assertTrue(true);
+        }
+    }
+
+    public void selectTabs() {
+        List<WebElement> Tabs = driver.findElements(VideoTabs);
+        for (int i = 0; i < Tabs.size(); i++) {
+            wait.hardWait(3);
+            String ele = Tabs.get(i).getText();
+            hoverelement();
+            scrollforward();
+            generic.click(Tabs.get(i));
+            if (Tabs.get(i).getAttribute("class").contains("selected")) {
+                wait.hardWait(3);
+                Assert.assertEquals(getText(selectedTab),ele,"Tab is selected");
+            }else {
+                Assert.fail(ele+" Tab is not selected");
+            }
+        }
+
+    }
+
+    public void hoverelement() {
+        WebElement Hoverele = driver.findElement(hoverEle);
+        mouseHoverToWebElement(Hoverele);
+    }
+
+    public void scrollforward() {
+        click(forwardscroll);
+    }
+
+    public void verifyVideoPlayed() {
+
+        WebElement Hoverele = driver.findElement(videoPage);
+        mouseHoverToWebElement(Hoverele);
+        wait.hardWait(3);
+        if ((getText(videoStartime) != "00:00") && (getText(videoEndtime) != "00:00")) {
+            Assert.assertTrue(true);
+        }
+        driver.navigate().back();
+        wait.hardWait(2);
+    }
+    public void clickOnVideos(){
+        List<WebElement> Tabs = driver.findElements(VideoTabs);
+        for (int i = 0; i < (Tabs.size()-1); i++) {
+            Tabs = driver.findElements(VideoTabs);
+            wait.hardWait(3);
+            generic.click(Tabs.get(i));
+            if (Tabs.get(i).getAttribute("class").contains("selected")) {
+                wait.hardWait(2);
+                videoSong_Play();
+                verifyVideoPlayed();
+            }
+        }
+    }
+}
